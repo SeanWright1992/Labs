11a12bc74d11de2d8bf6685bc08283cb1287a2f8
SeanWright1992 <38123765+SeanWright1992@users.noreply.github.com>
2018-04-30 14:45:16 -0700

    Update riscv1part2.asm

--- Lab 2/riscv1part2.asm
+++ Lab 2/riscv1part2.asm
@@ -11,32 +11,32 @@ Z:	.word 0
 
 	.text #Readable only section include executable code
 main:
-	la x1, A
-	#li x1, 10  #A
+	la x11, A
+	li x1, 10  #A
 	
-	la x2,B
-	#li x2, 15  #B
+	la x12,B
+	li x2, 15  #B
 	
-	la x3, C
-	#li x3, 6   #C
+	la x13, C
+	li x3, 6   #C
 	
-	la x4,Z
-	#li x4, 0   #z
+	la x31,Z
+	li x4, 0   #z
 	
 	addi x5, x3, 1 #add c + 1 for the second conditional statement in the second  else if statement
 	addi x6, x0, 7 #assigning  7 to x6 register in order to be incorporated in the second conditional statement in else if statement
 	addi x7, x0, 5 #assigning 5 to x7 register in order to be incorporated in the first conditional statement in the if statement.
 	li   x8,  1
 	
-	slt x14, x1, x2 #assigns 1 to x14 if A < B										The opposite of A < B which is B < A . Check if B not greater than A then its false to move on to else if statement.
-	beq x14, x0 Elseif #Jumps to Elseif if x14 ==0 (x14 == 0 when x1 > x2)					#check if z = 0 then set it equal to 1. 
-	slt x14, x7, x3 #assigns 1 to x14 if 5 < C							#the opposite of C > 5 in the first conditional statement in the if statement and move on to else if statement.
+	slt x14, x1, x2 #assigns 1 to x14 if A < B							
+	beq x14, x0 Elseif #Jumps to Elseif if x14 ==0 (x14 == 0 when x1 > x2)				 
+	slt x14, x7, x3 #assigns 1 to x14 if 5 < C							
 	beq x14, x0 Elseif #check if z = 0 then set it equal to 1. 
 	
 	j switchcase1
 	
 	Elseif: 
-	slt x15, x2, x1  #assigns 1 to x15 if B < A										#the opposite of  A > B which is  A < B.
+	slt x15, x2, x1  #assigns 1 to x15 if B < A										
 	beq x15, x8 switchcase2		#jumps to switch 2 if B >A
 	beq x6, x5, switchcase2		#jumps to switch 2 if 7 == C +1
 	j Else
@@ -60,6 +60,10 @@ switchcase3:
 	
 	
 	Exit:
+	sw x1,0(x11)
+	sw x2,0(x12)
+	sw x3,0(x13)
+	sw x4,0(x31)
 	li 	x4, 0
 	
 
